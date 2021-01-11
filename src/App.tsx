6f09c47f217e82938e41da445a74f587540c7e46
Jeremy Viloria <you@example.com>
2021-01-10 21:14:14 -0700

    added props to card

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
-import QuestionCard from './components/QuestionCard';
 // Components
+import QuestionCard from './components/QuestionCard';
+
+const TOTAL_QUESTIONS = 10;
 
 function App() {
 
@@ -31,7 +33,14 @@ function App() {
       <button className="start" onClick={startTrivia}>Start</button>
       <p className="score">Score:</p>
       <p>Loading Questions...</p>
-      <QuestionCard/>
+      <QuestionCard
+        questionNumber={number +1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number]  : undefined}
+        callback={checkAnswer}
+      />
       <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
