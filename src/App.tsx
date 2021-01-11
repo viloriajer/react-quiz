e39146d4c03d1e897b0a9c5a668f4dc02797185b
Jeremy Viloria <you@example.com>
2021-01-10 21:45:48 -0700

    Jessie wants me to take break

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import {fetchQuizQuestions} from './API';
 // Components
 import QuestionCard from './components/QuestionCard';
 // Types
-import {Difficulty} from './API';
+import {QuestionState, Difficulty} from './API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 const TOTAL_QUESTIONS = 10;
 
 function App() {
 
   const[loading, setLoading] = useState(false);
-  const[questions, setQuestions] = useState([]);
+  const[questions, setQuestions] = useState<QuestionState[]>([]);
   const[number,setNumber] = useState(0);
   const[userAnswers,setUserAnswers] = useState([]);
   const[score,setScore] = useState(0);
