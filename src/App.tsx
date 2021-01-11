cd9dfbbd42e5ff90864469f8a4c7add065f577c8
Jeremy Viloria <you@example.com>
2021-01-10 21:09:43 -0700

    Working on creating props

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import QuestionCard from './components/QuestionCard';
+// Components
 
 function App() {
+
+  const[loading, setLoading] = useState(false);
+  const[questions, setQuestions] = useState([]);
+  const[number,setNumber] = useState(0);
+  const[userAnswers,setUserAnswers] = useState([]);
+  const[score,setScore] = useState(0);
+  const[gameOver,setGameOver] = useState(true);
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+
+  }
+
+  const nextQuestion = () => {
+
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>Start</button>
+      <p className="score">Score:</p>
+      <p>Loading Questions...</p>
+      <QuestionCard/>
+      <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
 }
